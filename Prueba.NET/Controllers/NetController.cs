e68616af7026d49e8a7e8ba8dcaec2ee3b88dac9
DevWolf86 <romandev86@gmail.com>
2022-03-31 15:41:50 -0500

    Agregar archivos de proyecto.

---
+++ Prueba.NET/Controllers/NetController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DbPruebaNet;
+using Prueba.NET.Models;
+
+namespace Prueba.NET.Controllers
+{
+    public class Result
+    {
+        public string CC { get; set; }
+        public string SKU { get; set; }
+        public string F12 { get; set; }
+        public string OC { get; set; }
+        public string ESTADO { get; set; }
+
+
+    }
+
+    public class ResultModel
+    {
+      public string OC { get; set; }
+      public string PRECIO_DESPACHO { get; set; }
+      public string MONTO_TOTAL_LINEA { get; set; }
+      public string SKU_LINEA { get; set; }
+      public string F12 { get; set; }
+      public string TIPO_ABASTECIMIENTO { get; set; }
+      public string DUP_OC { get; set; }
+      public string DUP_F12 { get; set; }
+      public string ESTADO_LINEA { get; set; }
+      public string ESTADO_ORDEN_OMS { get; set; }
+      public string ESTADO_CD { get; set; }
+    }
+    public class NetController : Controller
+    {
+        // GET: Net
+        PruebaEntities db = new PruebaEntities();
+
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+            //PruebaEntities newdata = new PruebaEntities();
+            //return View(newdata.Datos_GetAll_New(""));
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Consulta(string documento)
+        {
+            //string documento = FormDocs["documento"];
+
+            //var lista = db.Datos.Where(x => x.CC == documento).ToList();
+            //Result result = new Result();
+            //result.CC = lista[0].CC;
+            //result.F12 = lista[0].F12;
+            //result.SKU = lista[0].SKU;
+            //result.OC = lista[0].OC;
+            //result.ESTADO = lista[0].ESTADO;
+
+            
+            PruebaEntities newdata = new PruebaEntities();
+            return View(newdata.Datos_GetAll_New(documento).AsEnumerable());
+
+
+          
+
+        }   
+    }
+}
---
+++ Prueba.NET/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba.NET.Models
+{
+    public class Listado
+    {
+        public List<Datos> Datas { get; set; }
+        public List<Cancelaciones> Cancelaciones { get; set; }
+        public List<Datos_GetAll_Result> Datos_GetAll_Results { get; set; }
+    }
+}
